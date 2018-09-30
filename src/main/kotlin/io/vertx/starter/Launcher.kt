b62d7926a3ee1c5f478a49ffd288ad0c90763ec3
Richard Gomez <rmgomez368@gmail.com>
2018-09-30 00:17:38 -0400

    Add main method

--- src/main/kotlin/io/vertx/starter/Launcher.kt
+++ src/main/kotlin/io/vertx/starter/Launcher.kt
@@ -1,17 +1,23 @@
 package io.vertx.starter
 
-import io.vertx.core.Launcher
 import io.vertx.core.Vertx
 import io.vertx.core.logging.Logger
 import io.vertx.core.logging.LoggerFactory
 
+object Launcher {
+  @JvmStatic
+  fun main(args: Array<String>) {
+    LauncherClass().dispatch(args)
+  }
+}
+
 /**
  * @author Richard Gomez
  */
-class Launcher : Launcher() {
+class LauncherClass : io.vertx.core.Launcher() {
 
   companion object {
-    val LOG: Logger = LoggerFactory.getLogger(Launcher::class.java)
+    val LOG: Logger = LoggerFactory.getLogger(LauncherClass::class.java)
   }
 
   override fun afterStartingVertx(vertx: Vertx?) {
